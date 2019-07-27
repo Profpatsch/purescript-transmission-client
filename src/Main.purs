9928d8fe470c7200eda5497b62b54fb4f3b25d8c
Profpatsch <mail@profpatsch.de>
2019-07-27 23:35:53 +0200

    Implement parsing of JSON response types

--- src/Main.purs
+++ src/Main.purs
@@ -2,6 +2,8 @@ module Main where
 
 import Prelude
 
+import Prim.RowList (class RowToList)
+import Control.Monad.Except (runExcept)
 import Data.Array.NonEmpty (NonEmptyArray)
 import Data.Array.NonEmpty as NonEmpty
 import Data.Either (Either(..))
@@ -14,6 +16,7 @@ import Effect.Aff as Aff
 import Effect.Class (liftEffect)
 import Effect.Console (log)
 import Effect.Console as Console
+import Foreign as Foreign
 import Foreign.Object (Object)
 import Foreign.Object as Object
 import Milkis as M
@@ -28,22 +31,52 @@ runQuery (Query f) tok = f tok
 derive instance functorQuery :: Functor Query
 
 -- A (json) request as expected by the transmission API
-type QueryRequest a = {
-  method :: String,
-  arguments :: a
-}
+type QueryRequest a =
+  { method :: String
+  , arguments :: a
+  }
+
+data QueryResponse a
+  = QueryResponseError String
+  | QueryResponseSuccess a
+
+data WithResponseData a = WithResponseData a
+
+instance readForeignQueryResponseNoData :: JSON.ReadForeign (QueryResponse Unit) where
+  readImpl f = do
+    s <- JSON.readImpl f :: Foreign.F { result :: String }
+    pure $ case s.result of
+      "success" -> do
+        QueryResponseSuccess unit
+      err -> QueryResponseError err
+
+instance readForeignQueryResponse :: (RowToList fields fieldList, JSON.ReadForeignFields fieldList () fields) => JSON.ReadForeign (QueryResponse { | fields } ) where
+  readImpl f = do
+    s <- JSON.readImpl f :: Foreign.F { result :: String }
+    case s.result of
+      "success" -> do
+        dat <- JSON.readImpl f :: Foreign.F { arguments :: { | fields } }
+        pure $ QueryResponseSuccess dat.arguments
+      err -> pure $ QueryResponseError err
 
 newtype Response a = Response (Either String a)
 
 newtype Id = Id Int
 
 derive newtype instance writeForeignId :: JSON.WriteForeign Id
+derive newtype instance readForeignId :: JSON.ReadForeign Id
+derive newtype instance showId :: Show Id
 
 data Which
   = AllTorrents
   | Ids (NonEmptyArray Id)
 
-torrentGet :: forall a. Which -> Query String
+type TorrentGetResponse =
+  { torrents :: Array Fields
+    -- TODO
+  }
+
+torrentGet :: Which -> Query (QueryResponse TorrentGetResponse)
 torrentGet which =
   query
     { method: "torrent-get"
@@ -57,7 +90,7 @@ torrentGet which =
 
 
 -- don’t forget to update fields
-data Fields = Fields {
+type Fields = {
   id :: Id
 }
 
@@ -75,35 +108,40 @@ main :: Effect Unit
 main = do
   let
     and (Left err) = log $ show err
-    and (Right res) = log $ show $ res
+    and (Right (QueryResponseError err)) = log $ show $ err
+    and (Right (QueryResponseSuccess res)) = log $ show $ res
 
   Aff.runAff_ and $ do
     { result } <- do
       { newToken } <- runQuery (torrentGet AllTorrents) dummyToken
-      runQuery (torrentGet AllTorrents) newToken
+      runQuery (torrentGet (AllTorrents)) newToken
     pure result
 
-query :: forall a. JSON.WriteForeign a => QueryRequest a -> Query String
-query q = Query \tok -> do
+query :: forall a b. JSON.WriteForeign a => JSON.ReadForeign (QueryResponse b) => QueryRequest a -> Query (QueryResponse b)
+query q = Query $ \tok -> do
   qRes <- query_ q tok
 
   -- TODO: handle HTTP errors
   --  * 405 method not allowed
   --  * …
-  Tuple newTok res <- case qRes of
-    Success a -> pure $ Tuple tok a
-    OtherHttpError resp -> pure $ Tuple tok resp
+  case qRes of
+    Success a -> pure $ { result: a, newToken: tok }
+    OtherHttpError resp ->
+      -- TODO: throw to outside, probably important
+      Aff.throwError $ Aff.error $ "transmission query: Got http error response!!\n" <> show (M.headers resp)
+    BodyDecodeError errs ->
+      Aff.throwError $ Aff.error $ "transmission query: unable to decode JSON body" <> show errs
     CSRFTokenInvalid { newToken } -> do
       qRes' <- query_ q newToken
       case qRes' of
         CSRFTokenInvalid _ ->
           Aff.throwError $ Aff.error "transmission query: renewed CSRFToken was also invalid!"
-        Success a -> pure $ Tuple newToken a
-        OtherHttpError resp -> pure $ Tuple newToken resp
-
-  body <- M.text res
-  let head = M.headers res
-  pure $ { result: (show head <> "\n" <> body), newToken: newTok }
+        OtherHttpError resp ->
+          -- TODO: throw to outside, probably important
+          Aff.throwError $ Aff.error $ "transmission query: Got http error response!!\n" <> show (M.headers resp)
+        BodyDecodeError errs ->
+          Aff.throwError $ Aff.error $ "transmission query: unable to decode JSON body" <> show errs
+        Success a ->pure $ { result: a, newToken }
 
 
 data QueryHttpResult a
@@ -112,13 +150,18 @@ data QueryHttpResult a
      newToken :: CSRFToken
   }
   | OtherHttpError M.Response
+  | BodyDecodeError Foreign.MultipleErrors
 
 -- TODO: add test for the csrf token stuff
-query_ :: forall a. JSON.WriteForeign a => QueryRequest a -> CSRFToken -> Aff (QueryHttpResult M.Response)
+query_ :: forall a b. JSON.WriteForeign a => JSON.ReadForeign (QueryResponse b) => QueryRequest a -> CSRFToken -> Aff (QueryHttpResult (QueryResponse b))
 query_ qt tok = do
   resp <- queryRaw qt tok
   case M.statusCode resp of
-    200 -> pure $ Success resp
+    200 -> do
+      body <- M.json resp
+      pure $ case runExcept $ JSON.readImpl body of
+                  Left errs -> BodyDecodeError errs
+                  Right res -> Success res
     409 -> do
       newTok <- case Object.lookup "x-transmission-session-id" $ M.headers resp of
             Nothing -> do
