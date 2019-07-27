18a7ca47ec212af7188a820abe9f9abe7a6cb268
Profpatsch <mail@profpatsch.de>
2019-07-27 21:04:46 +0200

    Pass through the QueryRequest and encode it to json

--- src/Main.purs
+++ src/Main.purs
@@ -2,10 +2,11 @@ module Main where
 
 import Prelude
 
+import Data.Array.NonEmpty (NonEmptyArray)
+import Data.Array.NonEmpty as NonEmpty
 import Data.Either (Either(..))
 import Data.List (List)
 import Data.Maybe (Maybe(..), isJust)
-import Data.NonEmpty (NonEmpty)
 import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Effect.Aff (Aff)
@@ -17,16 +18,43 @@ import Foreign.Object (Object)
 import Foreign.Object as Object
 import Milkis as M
 import Milkis.Impl.Node (nodeFetch)
+import Simple.JSON as JSON
 
-data Query a = Query a
+newtype Query a = Query (CSRFToken -> Aff { newToken :: CSRFToken, result :: a })
+
+runQuery :: forall a. Query a -> CSRFToken -> Aff { newToken :: CSRFToken, result :: a }
+runQuery (Query f) tok = f tok
+
+derive instance functorQuery :: Functor Query
+
+-- A (json) request as expected by the transmission API
+type QueryRequest a = {
+  method :: String,
+  arguments :: a
+}
 
 newtype Response a = Response (Either String a)
 
 newtype Id = Id Int
 
-data Get
+derive newtype instance writeForeignId :: JSON.WriteForeign Id
+
+data Which
   = AllTorrents
-  | Ids (NonEmpty List Id)
+  | Ids (NonEmptyArray Id)
+
+torrentGet :: forall a. Which -> Query String
+torrentGet which =
+  query
+    { method: "torrent-get"
+    , arguments:
+      { fields: requestedFields
+      , ids: case which of
+          AllTorrents -> Nothing
+          Ids ids -> Just $ NonEmpty.toArray ids
+      }
+    }
+
 
 -- don’t forget to update fields
 data Fields = Fields {
@@ -36,8 +64,8 @@ data Fields = Fields {
 -- For now we’ll just gonna list all of the manually.
 -- Late, do some row polymorphism magic to determine the
 -- response type from the type-level list of fields
-fields :: Array String
-fields = [ "id" ]
+requestedFields :: Array String
+requestedFields = [ "id", "files" ]
 
 newtype CSRFToken = CSRFToken String
 dummyToken :: CSRFToken
@@ -50,11 +78,13 @@ main = do
     and (Right res) = log $ show $ res
 
   Aff.runAff_ and $ do
-    Tuple res _ <- query (Query unit) dummyToken >>= \(Tuple _ tok) -> query (Query unit) tok
-    pure res
+    { result } <- do
+      { newToken } <- runQuery (torrentGet AllTorrents) dummyToken
+      runQuery (torrentGet AllTorrents) newToken
+    pure result
 
-query :: forall a. Query a -> CSRFToken -> Aff (Tuple String CSRFToken)
-query q tok = do
+query :: forall a. JSON.WriteForeign a => QueryRequest a -> Query String
+query q = Query \tok -> do
   qRes <- query_ q tok
 
   -- TODO: handle HTTP errors
@@ -73,7 +103,7 @@ query q tok = do
 
   body <- M.text res
   let head = M.headers res
-  pure $ Tuple (show head <> "\n" <> body) newTok
+  pure $ { result: (show head <> "\n" <> body), newToken: newTok }
 
 
 data QueryHttpResult a
@@ -84,9 +114,9 @@ data QueryHttpResult a
   | OtherHttpError M.Response
 
 -- TODO: add test for the csrf token stuff
-query_ :: forall a. Query a -> CSRFToken -> Aff (QueryHttpResult M.Response)
-query_ q tok = do
-  resp <- queryRaw q tok
+query_ :: forall a. JSON.WriteForeign a => QueryRequest a -> CSRFToken -> Aff (QueryHttpResult M.Response)
+query_ qt tok = do
+  resp <- queryRaw qt tok
   case M.statusCode resp of
     200 -> pure $ Success resp
     409 -> do
@@ -101,14 +131,14 @@ query_ q tok = do
       pure $ CSRFTokenInvalid { newToken: newTok }
     _ -> pure $ OtherHttpError resp
 
-queryRaw :: forall a. Query a -> CSRFToken -> Aff M.Response
-queryRaw q (CSRFToken tok) = do
+queryRaw :: forall a. JSON.WriteForeign a => QueryRequest a -> CSRFToken -> Aff M.Response
+queryRaw req (CSRFToken tok) = do
   let
     opts =
       { method: M.postMethod
       , headers: M.makeHeaders
           { "X-Transmission-Session-Id": tok }
-      , body: "{ \"method\": \"torrent-get\", \"arguments\": { \"fields\": [\"id\", \"eta\", \"files\"], \"id\": 10 } }"
+      , body: JSON.writeJSON req
       }
   -- TODO
   fetch (M.URL "http://localhost:9091/transmission/rpc") opts
